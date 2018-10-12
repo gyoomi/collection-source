e542f8db407cd69da915b6696451309bfec1cce7
gyoomi <gyoomi0709@foxmail.com>
2018-10-12 23:04:33 +0800

    HashSet初研究

---
+++ src/main/java/com/gyoomi/source/HashMapDemo.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2018, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.source;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/10/12 22:33
+ */
+public class HashMapDemo {
+
+    public static void main(String[] args) {
+        Map<String, String> map = new HashMap<>();
+    }
+}
---
+++ src/main/java/com/gyoomi/source/HashSetDemo.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright © 2018, LeonKeh
+ * <p>
+ * All Rights Reserved.
+ */
+
+package com.gyoomi.source;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 类功能描述
+ *
+ * @author Leon
+ * @version 2018/10/12 22:20
+ */
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+        Set set = new HashSet();
+    }
+
+
+}
