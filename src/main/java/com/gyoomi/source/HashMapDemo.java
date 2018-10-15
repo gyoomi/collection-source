d82aa48797fde35f0556d0310627e9a65fa5164e
gyoomi <gyoomi0709@foxmail.com>
2018-10-15 14:34:33 +0800

    list source commit

--- src/main/java/com/gyoomi/source/HashMapDemo.java
+++ src/main/java/com/gyoomi/source/HashMapDemo.java
@@ -19,5 +19,9 @@ public class HashMapDemo {
 
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<>();
+        map.put("123", "这123是值");
+        map.put("243", "4234");
+        map.put("23434", "2regetg");
+        Map<String, String> map2 = new HashMap<>(map);
     }
 }
