46c05a3c76870dc97feac900fb5043a7827bae2f
gyoomi <gyoomi0709@foxmail.com>
2018-10-24 11:23:33 +0800

    二分搜索树-基础添加

--- src/main/java/com/gyoomi/tree/BST.java
+++ src/main/java/com/gyoomi/tree/BST.java
@@ -17,16 +17,6 @@ public class BST<E extends Comparable<E>> {
     private Node root;
     private int size;
 
-    class Node {
-        public E e;
-        public Node left, right;
-        public Node(E e) {
-            this.e = e;
-            left = null;
-            right = null;
-        }
-    }
-
     public BST() {
         root = null;
         size = 0;
@@ -38,6 +28,48 @@ public class BST<E extends Comparable<E>> {
 
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    /**
+     * 向二分搜索树中添加元素
+     *
+     * @param e
+     */
+    public void add(E e) {
+        if (root == null) {
+            root = new Node(e);
+        } else {
+            add(root, e);
+        }
+    }
+
+    private void add(Node node, E e) {
+        if (node.e.equals(e)) {
+            return;
+        } else if (e.compareTo(node.e) > 0 && node.right == null) {
+            node.right = new Node(e);
+            size++;
+            return;
+        } else if (e.compareTo(node.e) < 0 && node.left == null) {
+            node.left = new Node(e);
+            size++;
+            return;
+        }
+        if (e.compareTo(node.e) > 0) {
+            add(node.right, e);
+        } else {
+            add(node.left, e);
+        }
+    }
+
+    class Node {
+        public E e;
+        public Node left, right;
+        public Node(E e) {
+            this.e = e;
+            left = null;
+            right = null;
+        }
     }
 
 }
